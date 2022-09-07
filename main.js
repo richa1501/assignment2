6d889ae3ee50335f1a4c0e1149d50865457f2217
Richa Dwivedi <richa1501ss@gmail.com>
2022-09-08 01:17:37 +0530

    test

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
-const library = [  {
-    author: 'Bill Gates',  title: 'The Road Ahead',  readingStatus: true
-},   {
-    author: 'Steve Jobs',  title: 'Walter Isaacson',  readingStatus: true
-},   {
-    author: 'Suzanne Collins',  title:  'Mockingjay: The Final Book of The Hunger Games',  readingStatus: false
+let library = [{
+    author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true
+}, {
+    author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true
+}, {
+    author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false
 }];
-   let tbody =document.querySelector("tbody");
-    let row =  library.map((e) =>{
-    let td= document.createElement('tr');
-        let author = `<td>${e.author}</td>`;
-        let title = `<td>${e.title}</td>`;
-        let readingStatus = `<td>${e.readingStatus}</td>`;
-        td.innerHTML = author + title + readingStatus;
-        return td;
-    });
-  
-  
-   
+library.sort((a, b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0))   // Sort Title here alphabetical
+
+let tbody = document.querySelector("tbody");
+let row = library.map((e) => {
+    let td = document.createElement('tr'); //To Creating New Row 
+    let author = `<td>${e.author}</td>`;
+    let title = `<td>${e.title}</td>`;
+    let readingStatus = `<td>${e.readingStatus}</td>`;
+    td.innerHTML = author + title + readingStatus;
+    return td;
+});
 row.forEach((n) => tbody.append(n));
-for (let i = 0; i < library.length; i++) 
-   {
-  
-    let book = "'" + library[i].title + "'" + ".";
-    if (library.readingStatus === true) {
-      console.log("Already read " + book);
-    } else
-    {
-     console.log("You still need to read " + book);
-    }
-   }
+
+//To print OutPut On Consol
+library.forEach(libraryItem => {
+    let book = `Title of this books is ${libraryItem.title} and Author of this book is ${libraryItem.author}`;
+
+    (library.readingStatus === true) ? console.log(`${book} i have read this book`) : console.log(`${book} still i need to read this book`);
+
+});
